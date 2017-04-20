204ed4d49f8edfd4a0242f35e6500c7b0f4511e4
Daragos <dragoscarlan83@gmail.com>
2017-04-20 22:50:57 +0100

    primproiect

---
+++ FirstProject/FirstProject/FirstProject.Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace FirstProject.Droid
+{
+	[Activity (Label = "FirstProject", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new FirstProject.App ());
+
+
+            int result=suma(1, 1);
+            int result1 = suma(result, 1);
+            
+		}
+
+        int suma(int x1, int x2)
+        {
+            return x1 + x2;
+        }
+	}
+}
+
